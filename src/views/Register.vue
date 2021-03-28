91c4c70bf66edbfc3ab5d6d342e181a2e43fbd9e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-28 14:45:10 -0300

    add register view

---
+++ src/views/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <layout-guest :invert="true">
+    <form>
+      <h2>Registre-se no Mynotes</h2>
+      <p>Cria uma conta agora</p>
+      <label for="form-email">Email</label>
+      <input type="email" id="form-email" />
+      <label for="form-confirm-password">Password</label>
+      <input type="password" id="form-password" />
+      <label for="form-confirm-password">Confirme o Password</label>
+      <input type="password" id="form-confirm-password" />
+      <button>Criar</button>
+      <router-link to="/">
+        Entre no app
+      </router-link>
+    </form>
+  </layout-guest>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  components: {
+    LayoutGuest: () => import('@/components/LayoutGuest')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/_form.scss';
+
+form {
+  h2 {
+    font-family: $font-primary;
+    font-weight: 700;
+  }
+  p {
+    margin-bottom: 16px;
+  }
+
+  a {
+    margin-top: 20px;
+  }
+
+  @media (min-width: 744px) {
+    flex: 1;
+  }
+}
+</style>
