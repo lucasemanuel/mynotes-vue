887a4ad075659e1459b07a544ea9234bdd48394e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-28 14:45:45 -0300

    add register router

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,12 @@ const routes = [
   {
     path: '/',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: () => import('@/views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/views/Register.vue')
   }
 ]
 
