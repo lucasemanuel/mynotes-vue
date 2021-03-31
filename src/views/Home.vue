1590267334b07f45f328392f3e3f6012bfabf52d
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-31 16:06:47 -0300

    update nav and layout responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="layout">
     <nav>
-      <h1 id="logo">Mynotes</h1>
-      <span>Favoritas</span>
-      <form>
-        <input type="text" placeholder="Ache suas anotações" />
-      </form>
-      <span>Sair</span>
+      <div class="container">
+        <Logo size="24px" />
+        <span>Sair</span>
+      </div>
     </nav>
-    <main class="container">
-      <note
-        content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+    <main>
+      <div class="container">
+        <note
+          content="assumenda laudantium labore qui dolores officia consectetur aquia quis
           placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
           adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
           sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
@@ -19,10 +18,90 @@
           vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
           mollitia tempora sed iusto enim. Beatae perferendis similique tempora
           nisi non!"
-        isFavorite="1"
-        createdAt="09/12/2020 - 21:17"
-        updatedAt="10/12/2020 - 04:58"
-      />
+          :isFavorite="true"
+          createdAt="09/12/2020 - 21:17"
+          updatedAt="10/12/2020 - 04:58"
+        />
+        <note
+          content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+          :isFavorite="true"
+          createdAt="09/12/2020 - 21:17"
+          updatedAt="10/12/2020 - 04:58"
+        />
+        <note
+          content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+          :isFavorite="false"
+          createdAt="09/12/2020 - 21:17"
+          updatedAt="10/12/2020 - 04:58"
+        />
+        <note
+          content="assumenda laudanreuiruieuriueiourweriuewirj
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto eniewre wtes magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beataem. Beatae perferendis similique tempora
+          nisi non!"
+          :isFavorite="false"
+          createdAt="09/12/2020 - 21:17"
+          updatedAt="10/12/2020 - 04:58"
+        />
+        <note
+          content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptaewre wtes magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Rsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptaewre wtes magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+          :isFavorite="true"
+          createdAt="09/12/2020 - 21:17"
+          updatedAt="10/12/2020 - 04:58"
+        />
+        <note
+          content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reice wrew rwr ewr wriendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+          :isFavorite="false"
+          createdAt="09/12/2020 - 21:17"
+          updatedAt="10/12/2020 - 04:58"
+        />
+      </div>
     </main>
   </div>
 </template>
@@ -30,31 +109,55 @@
 <script>
 export default {
   components: {
+    Logo: () => import('@/components/Logo'),
     Note: () => import('@/components/Note')
   }
 }
 </script>
 
 <style lang="scss" scoped>
-$width-container: 1200px;
+@mixin container {
+  max-width: 1200px;
+  padding: 0 15px;
+}
 
 #layout {
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
+  height: 100vh;
+  overflow: hidden;
   background: #e2e7e7;
 
   nav {
+    background-color: $color-primary;
+
+    div.container {
+      @include container;
+      display: flex;
+      justify-content: space-between;
+      line-height: 54px;
+      margin: 0 auto;
+
+      * {
+        font-family: $font-primary;
+        font-weight: 600;
+        color: #fff;
+        font-size: 24px;
+      }
+    }
   }
 
-  main.container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: $width-container;
-    min-height: calc(100vh - 56px - 24px);
-    margin: 24px auto;
-    padding: 15px;
+  main {
+    overflow-y: auto;
+    height: calc(100vh - 54px);
+
+    div.container {
+      @include container;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 24px auto;
+    }
   }
 }
 </style>
