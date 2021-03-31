d1760dc5d12af0265c8858450c5ebdbba1cf9ef6
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-31 15:07:20 -0300

    add logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1 class="logo" :style="style"><span>My</span>notes</h1>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    size: {
+      type: String
+    },
+    color: {
+      type: String
+    }
+  },
+  computed: {
+    fontSize () {
+      return this.size ? 'font-size: ' + this.size + ';' : null
+    },
+    textColor () {
+      return this.color ? 'color:' + this.color + ';' : null
+    },
+    style () {
+      return this.fontSize + this.textColor
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  font-family: $font-primary;
+  text-transform: uppercase;
+
+  span {
+    color: inherit;
+    font-family: inherit;
+    font-weight: 400;
+  }
+}
+</style>
