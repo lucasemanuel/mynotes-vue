62ce306a4502d7083dbaa41bb19ff035e64b1b3c
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-03-31 16:07:08 -0300

    add router to home

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home.vue')
+  },
+  {
+    path: '/login',
     name: 'Login',
     component: () => import('@/views/Login.vue')
   },
