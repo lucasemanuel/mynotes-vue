639228e97f1fde9736d97d69e030f85e56d5f00c
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-01 16:59:06 -0300

    add note list component

---
+++ src/components/NoteList.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <note
+      content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+      :isFavorite="true"
+      createdAt="09/12/2020 - 21:17"
+      updatedAt="10/12/2020 - 04:58"
+    />
+    <note
+      content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+      :isFavorite="true"
+      createdAt="09/12/2020 - 21:17"
+      updatedAt="10/12/2020 - 04:58"
+    />
+    <note
+      content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+      :isFavorite="false"
+      createdAt="09/12/2020 - 21:17"
+      updatedAt="10/12/2020 - 04:58"
+    />
+    <note
+      content="assumenda laudanreuiruieuriueiourweriuewirj
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptates magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto eniewre wtes magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beataem. Beatae perferendis similique tempora
+          nisi non!"
+      :isFavorite="false"
+      createdAt="09/12/2020 - 21:17"
+      updatedAt="10/12/2020 - 04:58"
+    />
+    <note
+      content="assumenda laudantium labore qui dolores officia consectetur aquia quis
+          placeat delectus eaque magnam. Lorem ipsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptaewre wtes magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Rsum dolor sit amet consectetu1.
+          adipisicing elit. Reiciendis voluptaewre wtes magnam maxime eius. Laborum,
+          sunt incidunt laboriosam neque blanditiis id accusantium eius omnis,
+          remm quia nulla, reprehenderit recusandae repellat libero! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Repudiandae officiis2
+          vel222 quaerat ipsam??? Praesentium aperiam, quam natus dolore illum
+          mollitia tempora sed iusto enim. Beatae perferendis similique tempora
+          nisi non!"
+      :isFavorite="true"
+      createdAt="09/12/2020 - 21:17"
+      updatedAt="10/12/2020 - 04:58"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoteList',
+  components: {
+    Note: () => import('@/components/Note')
+  }
+}
+</script>
+
+<style></style>
