c6a7bf554429d92b2e5ee76fc108444b7572a04e
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-02 16:54:35 -0300

    load mixins style

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: '@import "@/styles/_variables.scss";'
+        prependData: `
+          @import "@/styles/_mixins.scss";
+          @import "@/styles/_variables.scss";
+        `
       }
     }
   }
