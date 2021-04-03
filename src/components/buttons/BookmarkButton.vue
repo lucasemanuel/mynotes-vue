7ac932590ef435e4f4b59ace213640800697c72d
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-03 18:28:57 -0300

    refactor and create components for trash and bookmark buttons

---
+++ src/components/buttons/BookmarkButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button class="button-bookmark button" :class="{ favorited: favorited }">
+    <font-awesome-icon :icon="iconBookmark" />
+  </button>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  name: 'BookmarkButton',
+  components: {
+    FontAwesomeIcon
+  },
+  props: {
+    favorited: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      iconBookmark: faBookmark
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  @include buttonIcon;
+  @include buttonIconColor(#fff, $color-primary);
+
+  &:hover {
+    @include buttonIconColor($color-primary, #fff);
+  }
+
+  &.favorited {
+    @include buttonIconColor($color-primary, #fff);
+
+    &:hover {
+      @include buttonIconColor(#fff, $color-primary);
+    }
+  }
+}
+</style>
---
+++ src/components/buttons/TrashButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button-trash button">
+    <font-awesome-icon :icon="iconTrash" />
+  </button>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  name: 'TrashButton',
+  components: {
+    FontAwesomeIcon
+  },
+  data () {
+    return {
+      iconTrash: faTrash
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  @include buttonIcon;
+  @include buttonIconColor($color-danger, #fff);
+
+  &:hover {
+    @include buttonIconColor(#fff, $color-danger);
+  }
+}
+</style>
