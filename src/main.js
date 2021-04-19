5aa0f92b0341ff79d0f3f21fb8002156ad6a413b
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-19 17:18:18 -0300

    setting vee-validate

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import api from '@/services/api'
+
+import { extend } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
+import { messages } from 'vee-validate/dist/locale/pt_BR.json'
+
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule],
+    message: messages[rule]
+  })
+})
 
 Vue.config.productionTip = false
+Vue.prototype.$http = api
 
 new Vue({
   router,
