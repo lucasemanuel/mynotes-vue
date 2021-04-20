5ea0bb74b5a1b6f3d9e2cbe1c50da3b9a6b0432c
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-04-20 15:43:18 -0300

    add my custom toast component

---
+++ src/components/Toast.vue
@@ -0,0 +1,83 @@
+<template>
+  <div :class="[`alert-${color}`]" class="alert" role="alert">
+    <span class="message">
+      {{ message }}
+    </span>
+    <button
+      type="button"
+      class="close"
+      data-dismiss="alert"
+      aria-label="Close"
+      @click="$emit('remove')"
+    >
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import ToastMixin from 'vue-my-toasts/src/mixins/ToastMixin'
+import TimerMixin from 'vue-my-toasts/src/mixins/TimerMixin'
+
+export default {
+  name: 'BootstrapComponent',
+  mixins: [ToastMixin, TimerMixin],
+  props: {
+    title: {
+      type: [String, Boolean],
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    color () {
+      switch (this.type) {
+        case 'base':
+          return 'info'
+        case 'warning':
+          return 'warning'
+        case 'error':
+          return 'danger'
+        case 'success':
+          return 'success'
+        default:
+          return 'info'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.alert {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 16px;
+  padding: 16px;
+  border-radius: 4px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.24);
+
+  &-danger {
+    background: $color-danger;
+  }
+
+  &-success {
+    background: $color-primary;
+  }
+
+  .message {
+    margin-right: 8px;
+    color: #fff;
+  }
+
+  button {
+    background: none;
+    font-size: 24px;
+
+    span {
+      color: #fff;
+      font-weight: bold;
+    }
+  }
+}
+</style>
