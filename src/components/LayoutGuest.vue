2fa14469b56dfdc761db4461d91daf3cf5eb96f2
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-02 06:55:37 -0300

    clear layout using slot

--- src/components/LayoutGuest.vue
+++ src/components/LayoutGuest.vue
@@ -1,44 +1,16 @@
 <template>
   <div id="layout">
-    <main class="card" v-bind:class="styleClass">
-      <aside>
-        <logo />
-        <p class="subtitle">Nosso app de notas favorito!</p>
-        <p class="about">
-          Tenha suas notas salvas e acesse elas de onde e quando quizer
-        </p>
-      </aside>
-      <section>
-        <slot />
-      </section>
-    </main>
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LayoutGuest',
-  components: {
-    Logo: () => import('@/components/Logo')
-  },
-  props: {
-    invert: {
-      type: Boolean,
-      default: false
-    }
-  },
-  computed: {
-    styleClass: function () {
-      return {
-        'flex-row': !this.invert,
-        'flex-row-reverse': this.invert
-      }
-    }
-  }
+  name: 'LayoutGuest'
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #layout {
   display: flex;
   align-items: center;
@@ -46,87 +18,10 @@ export default {
   background: #e2e7e7;
   overflow-y: auto;
 
-  p {
-    font-size: 18px;
-    font-weight: 300;
-  }
-
-  .card {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    width: 100%;
-    box-shadow: 0 16px 80px rgba(0, 0, 0, 0.32);
-
-    aside,
-    section {
-      padding: 24px;
-    }
-
-    aside {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      background: $color-primary;
-      color: #fff;
-      text-align: center;
-
-      .logo,
-      span,
-      .about,
-      .subtitle {
-        color: inherit;
-      }
-
-      .logo {
-        font-size: 38px;
-      }
-
-      .about {
-        margin-top: 32px;
-        padding: 0 24px;
-        font-size: 20px;
-      }
-    }
-
-    section {
-      flex: 1;
-      background-color: #fff;
-    }
-  }
-
   @media (min-width: 744px) {
     justify-content: center;
     height: initial;
     min-height: 100vh;
-
-    .flex-row-reverse {
-      flex-direction: row-reverse;
-    }
-
-    .flex-row {
-      flex-direction: row;
-    }
-
-    .card {
-      height: 600px;
-      max-width: 888px;
-      margin: 12px;
-      overflow: auto;
-      border-radius: 4px;
-
-      aside {
-        width: calc(40 * 888px / 100);
-      }
-
-      section {
-        display: flex;
-        align-items: center;
-        flex: initial;
-        width: calc(60 * 888px / 100);
-        padding: 48px;
-      }
-    }
   }
 }
 </style>
