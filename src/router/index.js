899222dccf6d437336f6b55b53134ff1c1b342de
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-02 12:15:58 -0300

    routes for reset password

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,37 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/Home.vue')
+    component: () => import('@/views/Home')
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('@/views/Login.vue')
+    component: () => import('@/views/Login')
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import('@/views/Register.vue')
+    component: () => import('@/views/Register')
+  },
+  {
+    path: '/password',
+    component: () => import('@/views/Password'),
+    children: [
+      {
+        path: 'send-mail',
+        name: 'SendMailResetPassword',
+        component: () => import('@/views/RequestResetPassword')
+      },
+      {
+        path: 'reset/:code',
+        name: 'ResetPassword',
+        component: () => import('@/views/ResetPassword')
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: () => import('@/views/NotFound')
   }
 ]
 
