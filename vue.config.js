eacb88419df60c354ecc04d3bd335846758c7357
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-06 19:57:32 -0300

    config app name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'MyNotes'
+      return args
+    })
+  },
   css: {
     loaderOptions: {
       scss: {
