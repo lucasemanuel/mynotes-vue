62feea4c37f5acabd7893f8d84d5d105f4fcc188
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-05-06 12:15:22 -0300

    add request for note update

--- src/services/api/modules/note.js
+++ src/services/api/modules/note.js
@@ -4,10 +4,16 @@ export const index = payload => {
   return api.get('/notes', { params: payload })
 }
 
-export const remove = id => {
-  return api.delete(`/notes/${id}`)
+export const update = ({ id, body }) => {
+  return api.put(`/notes/${id}`, {
+    body
+  })
 }
 
 export const mark = id => {
   return api.patch(`/notes/${id}`)
+}
+
+export const remove = id => {
+  return api.delete(`/notes/${id}`)
 }
